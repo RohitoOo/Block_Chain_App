af7e89227e0f56b976a66560658c6750bc0985d4
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-09 01:48:03 +0530

    Created Test For Application

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -6,13 +6,40 @@ pragma solidity ^0.4.2 ;
 
 //And Smoke test 
 
+// contract Election {
+//     // Read Candidate 
+//     string public candidate;
+//     // Store Candidate
+//     // Constructor 
+
+//     function Election () public {
+//         candidate = "Candidate 1";
+//     }
+// }
+
 contract Election {
-    // Read Candidate 
-    string public candidate;
-    // Store Candidate
-    // Constructor 
+// Model Candidate ( Description of Candidate Name, votes etc..)
+    //   uint == unassigned Integer 
+  struct Candidate {
+      uint id ; 
+      string name ;
+      uint voteCount ;
+  }
+  // Store Candidate 
+  // Fetch Candidate 
+  mapping(uint => Candidate ) public candidates; 
+  // Store Candidates Count ( No. Of Candidate In Elections )
+
+    uint public candidatesCount ; 
 
     function Election () public {
-        candidate = "Candidate 1";
+       addCandidate("Candidate1");
+       addCandidate("Candidate2");
     }
+
+    function addCandidate ( string _name) private {
+        candidatesCount ++ ; 
+        candidates[candidatesCount] = Candidate(candidatesCount, _name,  0 );
+    }
+
 }
