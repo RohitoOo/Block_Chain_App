15378e9e7bbbbc0715f2b253f52f2be6cc11429d
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-09 02:06:08 +0530

    Created Test - Check Each Candidate's Property Against Its Values

--- election/test/election.js
+++ election/test/election.js
@@ -2,6 +2,10 @@ var Election = artifacts.require("./Election.sol");
 
 contract("Election", function(accounts){
 
+    var electionInstance; 
+
+// Check to see if there are TWO candidates 
+
     it("initializes with two candidates" , function(){
         return Election.deployed().then(function (instance) {
             return instance.candidatesCount();
@@ -9,4 +13,24 @@ contract("Election", function(accounts){
             assert.equal(count, 2);
         });
     });
+
+// Check to see if the name, ID and Vote Count is correct 
+
+
+    it("it initializes the candidates with the correct Values" , function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            return electionInstance.candidates(1); 
+        }).then(function(candidate){
+            assert.equal(candidate[0] , 1 , "Containes the correct id"  );
+            assert.equal(candidate[1] , "Candidate1" , "Containes the correct Candidate's Name"  );
+            assert.equal(candidate[2] , 0 , "Containes the correct Count of Votes"  );
+            return electionInstance.candidates(2);  
+        }).then(function(candidate){
+            assert.equal(candidate[0] , 2 , "Containes the correct id"  );
+            assert.equal(candidate[1] , "Candidate2" , "Containes the correct Candidate's Name"  );
+            assert.equal(candidate[2] , 0 , "Containes the correct Count of Votes"  );
+        })
+    })
+
 });
